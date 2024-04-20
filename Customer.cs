8da788dd044943adb237ca1d1e623680d8ee9b23
Fanial-Woldu <112184323+Fanial-Woldu@users.noreply.github.com>
2024-04-20 13:36:47 -0500

    Add files via upload
    CSHTML files belong in View, CS Files belong in Models, and HomeController.cs belongs in Controller.

---
+++ Customer.cs
@@ -0,0 +1,123 @@
+﻿namespace NorthWindProject2024.Models
+{
+    //Ogedegbe, Ovo
+    public class Customer
+    {
+        private string customerId = "N/A";
+        private string companyName = "N/A";
+        private string contactName = "N/A";
+        private string contactTitle = "N/A";
+        private string address = "N/A";
+        private string city = "N/A";
+        private string region = "N/A";
+        private string postalCode = "N/A";
+        private string country = "N/A";
+        private string phone = "N/A";
+        private string fax = "N/A";
+
+        public string CustomerId
+        {
+            get { return this.customerId; }
+            set { this.customerId = value; }
+        }
+
+        public string CompanyName
+        {
+            get { return this.companyName; }
+            set { this.companyName = value; }
+        }
+
+        public string ContactName
+        {
+            get { return this.contactName; }
+            set { this.contactName = value; }
+        }
+
+        public string ContactTitle
+        {
+            get { return this.contactTitle; }
+            set { this.contactTitle = value; }
+        }
+
+        public string Address
+        {
+            get { return this.address; }
+            set { this.address = value; }
+        }
+
+        public string City
+        {
+            get { return this.city; }
+            set { this.city = value; }
+        }
+
+        public string Region
+        {
+            get { return this.region; }
+            set { this.region = value; }
+        }
+
+        public string PostalCode
+        {
+            get { return this.postalCode; }
+            set { this.postalCode = value; }
+        }
+
+        public string Country
+        {
+            get { return this.country; }
+            set { this.country = value; }
+        }
+
+        public string Phone
+        {
+            get { return this.phone; }
+            set { this.phone = value; }
+        }
+
+        public string Fax
+        {
+            get { return this.fax; }
+            set { this.fax = value; }
+        }
+
+
+        // Constructor
+        public Customer()
+        {
+
+        }
+
+        public Customer(string aCustomerId, string aCompanyName, string aContactName, string aContactTitle, string aAddress, string aCity, string aRegion, string aPostalCode, string aCountry, string aPhone, string aFax)
+        {
+            this.CustomerId = aCustomerId;
+            this.CompanyName = aCompanyName;
+            this.ContactName = aContactName;
+            this.ContactTitle = aContactTitle;
+            this.Address = aAddress;
+            this.City = aCity;
+            this.Region = aRegion;
+            this.PostalCode = aPostalCode;
+            this.Country = aCountry;
+            this.Phone = aPhone;
+            this.Fax = aFax;
+        }
+        /*
+        public override string ToString()
+        {
+            string message = "";
+            message = message + "Customer Id" + this.CustomerId + "<br />";
+            message = message + "Company Name" + this.CompanyName + "<br />";
+            message = message + "Contact Name" + this.ContactName + "<br />";
+            message = message + "Contact Title" + this.ContactTitle + "<br />";
+            message = message + "Address" + this.Address + "<br />";
+            message = message + "City" + this.City + "<br />";
+            message = message + "Region" + this.Region + "<br />";
+            message = message + "Postal Code" + this.PostalCode + "<br />";
+            message = message + "Country" + this.Country + "<br />";
+            message = message + "Phone" + this.Phone + "<br />";
+            message = message + "Fax" + this.Fax + "<br />";
+            return message;
+        }*/
+    }
+}
---
+++ OrderDetails.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NorthWindProject2024.Models
+{
+    ////Woldu, Fanial K
+    public class OrderDetails
+    {
+        
+        public int orderId = -1;
+        public int productId = -1;
+        private double unitPrice = Double.MaxValue;
+        private int quantity = -1;
+        private double discount = 0.0;
+
+        public int OrderId
+        {
+            get { return this.orderId; }
+            set { this.orderId = value; }
+        }
+
+        public int ProductId
+        {
+            get { return this.productId; }
+            set { this.productId = value; }
+        }
+
+        public double UnitPrice
+        {
+            get { return this.unitPrice; }
+            set { this.unitPrice = value; }
+        }
+
+        public int Quantity
+        {
+            get { return this.quantity; }
+            set { this.quantity = value; }
+        }
+
+        public double Discount
+        {
+            get { return this.discount; }
+            set { this.discount = value; }
+        }
+
+
+        //Constructor
+        public OrderDetails(int aOrderId, int aProductId, double aUnitPrice, int aQuantity, double aDiscount)
+        {
+            this.OrderId = aOrderId;
+            this.ProductId = aProductId;
+            this.UnitPrice = aUnitPrice;
+            this.Quantity = aQuantity;
+            this.Discount = aDiscount;
+        }
+    }
+}
